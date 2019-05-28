5a0fb2ff59466b8dc04ecae34d4d0292e5d5971f
conconrob <connortprobertson@gmail.com>
2019-05-28 14:49:10 +0100

    added items to room

--- src/room.py
+++ src/room.py
@@ -3,9 +3,10 @@
 
 
 class Room:
-    def __init__(self, name, description,):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
+        self.items = items
 
     def __getitem__(self, key):
         return getattr(self, key)
